fc2d8193482f348698c258d218992717e7d19f65
d1ck50n <dickstremism@ya.ru>
2024-06-29 16:23:17 +0300

    delete build dirs

--- tests/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/d1ck50n/projects/simple_cli/tests
-# Build directory: /home/d1ck50n/projects/simple_cli/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/Catch2")
--- tests/build/lib/Catch2/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
+++
@@ -1,4 +0,0 @@
-lib/Catch2/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
- /home/d1ck50n/projects/simple_cli/tests/lib/Catch2/src/catch2/internal/catch_result_type.cpp \
- /usr/include/stdc-predef.h \
- /home/d1ck50n/projects/simple_cli/tests/lib/Catch2/src/catch2/../catch2/internal/catch_result_type.hpp
